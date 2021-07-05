a96b27664b1d91f1adc29d04c87d909529a7f987
SQUID-DESKTOP\squid <squid@stormwind.it>
2021-07-05 20:36:19 +0200

    first commit

---
+++ src/main/java/com/github/tgiachi/sofa/sofaserver/SofaServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.tgiachi.sofa.sofaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tgiachi/sofa/sofaserver/SofaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tgiachi.sofa.sofaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
