f74b269f5d0465d06b2d4aa1b4218fd7199b4c09
Tom <tom@isendu.com>
2021-07-09 18:05:39 +0200

    Album Item

--- sofa-ui/src/components/album_item.tsx
+++ sofa-ui/src/components/album_item.tsx
@@ -2,7 +2,7 @@ import {AlbumEntity} from "../api/api.interfaces";
 import {Avatar, Box, Grid, Text} from "@chakra-ui/react"
 
 export const AlbumItem = ({album}: { album: AlbumEntity }) => {
-    return (<Grid templateColumns="repeat(2, 1fr)" style={{width: '300px'}} gap={6}>
+    return (<Grid templateColumns="repeat(2, 1fr)" style={{width: '800px'}} gap={6}>
         <Box w="20%" h="10" bg="blue.500">
             <Avatar name={album.name} src={album.coverUrl}/>
         </Box>
